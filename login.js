278635f1390c50e3ab1ef9f40afc0cc89f0352ec
tsuji.teruaki <tsuji@i-system.co.jp>
2025-07-15 17:14:54 +0900

    20250715

--- login.js
+++ login.js
@@ -29,7 +29,7 @@ function showOrHide() {//パスワードを表示するか隠すかを判断す�
     while (!found && i < userdata.length) {//foundがtrueを返すかuserdataのlengthがiより少なくなるまで以下の処理を実行
       loginuser = userdata[i];
       if (loginuser.hasOwnProperty("name") && loginuser.hasOwnProperty("password")) {//データが破損していないかの確認（参考:chatgpt）
-        if (loginuser.name === username && loginuser.password === password) {
+        if (loginuser.name == username && loginuser.password == password) {
           found = true;
         }
       }
